39c5170b04e08264a4688a4d4a557d65d4063bdd
Šarūnas Navickas <sarunas.navickas@ovoko.com>
2024-01-30 09:40:38 +0200

    Some code changes for easier debuging

--- wordnet_extract.py
+++ wordnet_extract.py
@@ -1,7 +1,10 @@
-import os.path
 import json
+import logging
+import os.path
 from argparse import ArgumentParser
 from itertools import chain
+
+logger = logging.getLogger(__name__)
 
 
 def parse_db(db_path, pos):
@@ -16,20 +19,51 @@ def parse_db(db_path, pos):
             "wordnet_id": word_id,
             "word": word,
             "key": "{}_{}".format(word, ref_count),
-            "synonyms": [other[i] for i in range(0, int(ref_count, 16) - 1, 2)]
+            "synonyms": [other[i] for i in range(0, int(ref_count, 16) - 1, 2)],
         }
         yield data
 
+
 def main(args):
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
     path = args.db_path
-    results = sorted(chain(parse_db(path, "adv"), parse_db(path, "adj"), parse_db(path, "noun"), parse_db(path, "verb")), key=lambda o: o["word"])
+    results = sorted(
+        chain(
+            parse_db(path, "adv"),
+            parse_db(path, "adj"),
+            parse_db(path, "noun"),
+            parse_db(path, "verb"),
+        ),
+        key=lambda o: o["word"],
+    )
     with open(args.output, "w") as f:
         for r in results:
-            f.write(json.dumps(r) + "\n")
+            logger.debug(r)
+            if not args.noop:
+                f.write(json.dumps(r) + "\n")
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--db_path", help="Directory where wordnet data files are located", default=".")
+    parser.add_argument(
+        "--db_path",
+        "--db-path",
+        help="Directory where wordnet data files are located",
+        default=".",
+    )
+    parser.add_argument(
+        "--debug", action="store_true", default=False, help="Show debug output"
+    )
+    parser.add_argument(
+        "--noop",
+        action="store_true",
+        default=False,
+        help="Don't write output to file. Use for debugging purposes",
+    )
     parser.add_argument("output", help="Output file, jsonl extension")
     main(parser.parse_args())
+
